366a6207c22d700f26fb88325351e81e0edec3d4
kin <kin@southpiece.net>
2025-04-30 20:49:58 +0900

    import

--- Sources/KinKit/KinKit.swift
+++ Sources/KinKit/KinKit.swift
@@ -4,3 +4,4 @@
 //
 //  Created by kin on 4/30/25.
 //
+@_exported import Foundation
--- Sources/KinKit_OtherPlatform/KinKit_OtherPlatform.swift
+++ Sources/KinKit_OtherPlatform/KinKit_OtherPlatform.swift
@@ -4,3 +4,6 @@
 //
 //  Created by kin on 4/30/25.
 //
+#if canImport(FoundationEssentials) && !os(macOS) && !os(iOS) && !os(tvOS) && !os(watchOS)
+@_exported import FoundationEssentials
+#endif
