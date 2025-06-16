5aad4ca05ed93fe5b97730a8df671c7f440546df
kin <kin@southpiece.net>
2025-06-16 15:45:33 +0900

    import 위치 변경

--- Sources/KinKitCore/Util.swift
+++ Sources/KinKitCore/Util.swift
@@ -7,16 +7,20 @@
 
 import Foundation
 
+#if os(iOS)
+import UIKit
+#elseif os(watchOS)
+import WatchKit
+#endif
+
 public func currentDeviceName() -> String {
     #if os(iOS)
-    import UIKit
     return UIDevice.current.name
     #elseif os(macOS)
     return Host.current().localizedName ?? "Unknown Mac"
     #elseif os(tvOS)
     return UIDevice.current.name
     #elseif os(watchOS)
-    import WatchKit
     return WKInterfaceDevice.current().name
     #else
     return "Unknown Device"
